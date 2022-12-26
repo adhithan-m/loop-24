dc51b2924edd1f12cb00d6a18c3d28d571ee73e4
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 20:06:27 +0530

    reverse.

---
+++ reverse/js/reverse.js
@@ -0,0 +1,48 @@
+//reverse number
+
+var n =parseInt(prompt("Enter the number"));
+ while(n>0){
+	 rem=n%10
+	 n=(n-rem)/10
+	 if(rem==1)
+	 {
+		 document.write("one"+"<br>")
+	 }
+	 else if(rem==2)
+	 {
+		 document.write("two"+"<br>")
+	 }
+	 else if(rem==3)
+	 {
+		 document.write("three"+"<br>")
+	 }
+	 else if(rem==4)
+	 {
+		 document.write("four"+"<br>")
+	 }
+	 else if(rem==5)
+	 {
+		 document.write("five"+"<br>")
+	 }
+	 else if(rem==6)
+	 {
+		 document.write("six"+"<br>")
+	 }
+	 else if(rem==7)
+	 {
+		 document.write("seven"+"<br>")
+	 }
+	 else if(rem==8)
+	 {
+		 document.write("eight"+"<br>")
+	 }
+	 else if(rem==9)
+	 {
+		 document.write("nine"+"<br>")
+     }
+	 else
+	 {
+		document.write("zero"+"<br>")
+     }
+ }*/
+ 
